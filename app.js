3d4711294e910eadef9470ae0a3f820a784ccaa0
naphattar <naphatchalermwai@gmail.com>
2023-03-04 14:24:32 +0700

    set for deployment

--- app.js
+++ app.js
@@ -12,16 +12,21 @@ dotenv.config();
 db.connect();
 
 const app = express();
+app.use(express.static('public'));
 app.use(express.json());
 //app.use(express.urlencoded({extended : true}));
 
 const corsOptions = {
-    origin: 'http://localhost:3000',
+    origin: 'guy2yowebpage.vercel.app',
     credentials: true,
 };
 
 app.use(cors(corsOptions));
 //
+
+app.get('/',(req,res) =>{
+    res.send("Server is working ... ");
+});
 
 app.get('/users' , async(req,res) =>{
     try{
