1bed7640845c5c5e4409af74e317bd3ce6971808
naphattar <naphatchalermwai@gmail.com>
2023-03-04 15:21:46 +0700

    remove credentials

--- app.js
+++ app.js
@@ -21,7 +21,6 @@ const corsOptions = {
     origin: 'https://guy2yowebpage.vercel.app', // use your actual domain name (or localhost), using * is not recommended
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
-    credentials: true
 };
 
 app.use(cors(corsOptions));
